a843e1e73d5b13208926a08b166113702b2c9622
Andrew DiBella <andrew.dibella1@marist.edu>
2020-02-25 23:19:50 -0500

    rest tomorrow

--- prj/2/Driver_prj2.java
+++ prj/2/Driver_prj2.java
@@ -14,26 +14,33 @@ class Driver_prj2{
     ArrayQueue qu = new ArrayQueue();
     Map<Location,Location> ml = new TreeMap<>();
 
+    // initialize start 
     qu.add(start);
+    ml.get(start).equals(start);
     
+
+    // checks if reached end location 
     while (!m.isEndLocation(qu.getFront())){
       while(true){
         Location front = new Location();
         front = qu.getFront();
 
         Location next = front.nextNeighbor();
-        
+        // if next is valid location add to qu
         if (m.isValidLocation(next)){
           qu.add(next);
-          ml.get(next).equals(front);
+          //if next is not already a key add shortest path to map
+          if(!ml.containsKey(next))
+            ml.get(next).equals(front);
 
         }
-
+        // if next is done checking neighbors break and restart loop
         if (next.isDone()){
           qu.remove();
           break;
         }
       }
+      // if qu is empty then no solution 
       if (qu.getLength() == 0) break;
     
     }
