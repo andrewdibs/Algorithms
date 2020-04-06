8f7cfac93270408fd4dd418fa54e8be26b0cf286
andrewdibs <Andrew.DiBella1@marist.edu>
2020-04-06 18:39:57 -0400

    hw4

--- hw/4/Hash.java
+++ hw/4/Hash.java
@@ -1,11 +1,49 @@
-class Hash{
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Hash {
   public static void main(String [] args){
-    hash_string_1(key, tableSize);
     
+    Scanner input = new Scanner(System.in);
+    int tableSize = 8192;
+    int probes = 0;
+    Map<Integer, String> map = new HashMap<>();
+    
+    while (input.hasNext()){
+      String next = input.nextLine();
+      
+
+      int index = hash_string_3(next, tableSize);
+      while (map.get(index) != null){
+        index++;
+        probes++;
+      }
+      map.put(index, next);
+      probes++;
+
+
+      // QUADRATIC Probing ; choose one commment other
+
+      int index = hash_string_3(next,tableSize);
+        int start = index;
+        int count = 1;
+        do{
+          if (map.get(index) == null){
+              map.put(index, next);
+              probes++;
+              break;
+            }
+            probes++;        
+            index = (index + count * count++) % tableSize;            
+        } while (index != start);  
 
 
 
+    }
+    System.out.println(probes); 
   }
+
   static int hash_string_1(String key, int tableSize) {
     return (key.length() * key.length() * 4) % tableSize;
   }
@@ -16,9 +54,13 @@ class Hash{
         729 * key.charAt(2)) % tableSize;
   }
   
-  //static int hash_string_3(String key, int tableSize) {
-    // come up with your own hash function and see if you 
-    // can do better on any of the test cases
-  //}
+  static int hash_string_3(String key, int tableSize) {
+    int hash = 0;
+    int prime = 31;
+    for (int i = 0; i < key.length();i++){
+      hash = (prime * hash + key.charAt(i));
+    }
+    return hash % tableSize;
+  }
 
 }
